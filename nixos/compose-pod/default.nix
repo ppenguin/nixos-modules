d46dfe597847061bf1247342870f4f70a4e45912
ppenguin <hieronymusv@gmail.com>
2023-10-12 20:06:32 +0200

    more experiments [still broken]

--- nixos/compose-pod/default.nix
+++ nixos/compose-pod/default.nix
@@ -34,11 +34,20 @@ let
   # Hm, it segfaults with
   #   `/run/wrappers/bin/newuidmap 577301 0 992 1 1 231072 65536`: Assertion `!(st.st_mode & S_ISUID) || (st.st_uid == geteuid())` in NixOS's wrapper.c failed.`
   # could this be the solution? https://github.com/NixOS/nixpkgs/pull/231673
-  compose-wrap = stateDir: (pkgs.writeShellApplication {
+  compose-wrap = (pkgs.writeShellApplication {
     name = "compose-wrap.sh";
-    runtimeInputs = with pkgs; [ podman-compose (podman.override { extraPackages = [ "/run/wrappers" ]; }) ]; 
+    runtimeInputs = with pkgs; [
+      podman-compose
+      (podman.override { extraPackages = [ "/run/wrappers" ]; })
+      fuse-overlayfs
+      libcap
+    ];
     text = ''
-      HOME="${stateDir}" podman-compose "$@"
+      printf "%s (PID=%s)\n" "$0" "$$"
+      echo "capabilities:"
+      getpcaps $$
+      env
+      cd "$HOME" && podman-compose "$@"
     '';
   }) + "/bin/compose-wrap.sh";
 
@@ -61,48 +70,83 @@ in {
     # We could maybe do it like here: https://gist.github.com/udf/4d9301bdc02ab38439fd64fbda06ea43
     # but this is only slightly less verbose...
 
+    security.wrappers = {
+      newuidmap = lib.mkForce {
+        program = "newuidmap";
+        source = "${pkgs.shadow.out}/bin/newuidmap";
+        # setuid = true;
+        permissions = "4511";
+        owner = "root";
+        group = "root";
+        # capabilities = "cap_setuid,cap_setgid,cap_net_raw+eip";
+        capabilities = "cap_setuid=eip";
+      };
+    };
+
     systemd.services = (mapAttrs' (podname: cfg: (
       let
         homeDir = "/var/lib/${cfg.user}";
       in
-      nameValuePair "pod-${podname}" { # here the systemd unit definition
+      nameValuePair "pod-${podname}" rec { # here the systemd unit definition
         enable = true;
         description = "Service for ${podname} pod";
         wantedBy = [ "multi-user.target" ];
         after = [ "network.target" ];
         # restartTriggers = [ cfg.apikeyFile cfg.apisecretFile ];
-        path = with pkgs; [
-          bash
+        path = with pkgs; [ # path doesn't seem to be available in the Exec? Or is it? env says not, but if we set environment.PATH we get "conflicting definition"
+          coreutils
           podman-compose
           (podman.override { extraPackages = [ "/run/wrappers" ]; })
+          "/run/wrappers"
+          dbus
+          bash
+          su # chage???
         ];
         environment = { # check whether we have variable expansion...
-          HOME = "${homeDir}";
+          # HOME = "${homeDir}";
+          # PATH = strings.concatMapStringsSep ":" (p: "${p}/bin:${p}/sbin") path;
           # XDG_DATA_DIRS = "${homeDir}/.local";
           # XDG_CONFIG_DIRS = "${homeDir}/.config";
-          XDG_RUNTIME_DIR = "/run/user/$UID";
-          # DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/$UID/bus";
-          CHECK_EXP = "%d%h";
+          XDG_RUNTIME_DIR = "/run/${cfg.user}";
+          # TMPDIR = "/run/${cfg.user}/tmp";
+          # DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/${cfg.user}/bus";
         };
         serviceConfig = {
           Type = "exec";
           LoadCredential = lib.lists.optional (cfg.envFile != null) [ "envfile:${cfg.envFile}" ];
           # Will be available to the systemd exec env as %d/envfile
+          ProtectHostname = "no";
           DynamicUser = true;
+          # CapabilityBoundingSet = "CAP_SETUID CAP_SETGID CAP_SETPCAP CAP_SETFCAP CAP_NET_RAW";
+          # CapabilityBoundingSet = "CAP_SETUID";
+          AmbientCapabilities = "CAP_SETUID CAP_SETGID CAP_SETPCAP CAP_SETFCAP CAP_NET_RAW"; # https://unix.stackexchange.com/a/581337/395327
+          RestrictSUIDSGID = false;
           StateDirectory = "${cfg.user}";
+          RuntimeDirectory = "${cfg.user}";
           User = "${cfg.user}";
           Group = "${cfg.group}";
-          ExecStartPre = ''
-            ${pkgs.bash}/bin/bash -c 'printf "UID=%%s\nHOME=%%s\nXDG_RUNTIME_DIR=%%s\nCHECK_EXP=%%s\n" "$UID" "$HOME" "$XDG_RUNTIME_DIR" "$CHECK_EXP"'
-          '';
+          ExecStartPre = [
+            ''${pkgs.bash}/bin/bash -c 'echo "UID=$UID"; env' ''
+            # ''${pkgs.dbus}/bin/dbus-run-session -- /usr/bin/env bash -c 'ls -laR /run/${cfg.user} /run/user/$UID; systemctl --user list-units' ''
+            # ''${pkgs.bash}/bin/bash -c 'ls -laR /run/${cfg.user}; systemctl --user is-active dbus || systemctl --user start dbus' ''
+          ];
+          #   ${pkgs.coreutils}/bin/env
+          # '';
+          # ExecStart = ''
+          #   ${compose-wrap} \
+          #     ${
+          #       optionalString (cfg.envFile != null) "--env-file=%d/envfile"
+          #     } \
+          #     --podman-args="--log-level debug" \
+          #     -p "${podname}" \
+          #     -f ${cfg.composeFile} \
+          #     up
+          # '';
+          # ExecStart = ''
+          #   ${pkgs.bash}/bin/bash -c 'cd $HOME && podman run -p 8080:80 docker.io/library/nginx:latest'
+          # '';
           ExecStart = ''
-            podman-compose \
-              ${
-                optionalString (cfg.envFile != null) "--env-file=%d/envfile"
-              } \
-              -p "${podname}" \
-              -f ${cfg.composeFile} \
-              up
+            ${pkgs.dbus}/bin/dbus-launch -- ${pkgs.podman}/bin/podman --runroot=/run/${cfg.user}/containers --root=${homeDir}/.local/share/containers/storage --tmpdir=/run/${cfg.user}/tmp run -p 8080:80 docker.io/library/nginx:latest
           '';
         };
       }
