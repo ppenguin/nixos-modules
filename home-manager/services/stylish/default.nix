e42cd259136a2c2ab99e095dd01ce04098e9df2f
ppenguin <hieronymusv@gmail.com>
2024-06-29 22:28:53 +0200

    improve stylish (HM) service module

--- home-manager/services/stylish/default.nix
+++ home-manager/services/stylish/default.nix
@@ -1,112 +1,120 @@
 # HM module for styli.sh service
 # Supports timed execution of styli.sh with (for now) selected options configurable
-self:
-{ config, pkgs, lib, ... }:
-
-let
+self: {
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
   cfg = config.services.stylish;
   prefixQuotedValOrEmptyNull = str: pfx:
-    if str != null then ''${pfx}"${str}"'' else "";
+    if str != null
+    then ''${pfx}"${str}"''
+    else "";
+in
+  with lib;
+  with builtins; {
+    imports = [];
 
-in with lib;
-with builtins;
+    ###### interface
 
-{
+    options = {
+      services.stylish = with types; {
+        enable = mkEnableOption "stylish";
 
-  imports = [ ];
+        package = mkOption {
+          type = types.package;
+          default = pkgs.stylish;
+          defaultText = literalExpression "pkgs.stylish";
+          example = literalExpression "pkgs.stylish";
+          description = "The package to use for the styli.sh script";
+        };
 
-  ###### interface
+        search = mkOption {
+          type = nullOr str;
+          default = null;
+          description = mdDoc ''
+            Search string (value used for `-s`|`--search` flag)
+          '';
+        };
 
-  options = {
+        flags = mkOption {
+          type = listOf str;
+          default = [];
+          description = mdDoc ''
+            Flags for `styli.sh` (list of string).
+            (See [`styli.sh` documentation](https://github.com/thevinter/styli.sh))
+          '';
+          example = literalExpression ''[ "-w 3840" "-h 2160" ]'';
+        };
 
-    services.stylish = with types; {
+        env = mkOption {
+          type = listOf str;
+          default = [];
+          description = mdDoc ''
+            Environment variables for styli.sh (list of string).
+            If you use (the default) unsplash as a wallpaper provider,
+            you should at least set UNSPLASH_ACCESS_KEY.
+          '';
+        };
 
-      enable = mkEnableOption "stylish";
-
-      package = mkOption {
-        type = types.package;
-        default = pkgs.stylish;
-        defaultText = literalExpression "pkgs.stylish";
-        example = literalExpression "pkgs.stylish";
-        description = "The package to use for the styli.sh script";
+        refreshInterval = mkOption {
+          default = "0";
+          type = str;
+          description = mdDoc ''
+            If set, `styli.sh` will be executed with this interval to refresh the wallpaper.
+            (Interval in the format of [`systemd.time`](https://www.freedesktop.org/software/systemd/man/systemd.time.html))
+          '';
+          example = literalExpression "30min";
+        };
       };
-
-      search = mkOption {
-        type = nullOr str;
-        default = null;
-        description = mdDoc ''
-          Search string (value used for `-s`|`--search` flag)
-        '';
-      };
-
-      flags = mkOption {
-        type = listOf str;
-        default = [ ];
-        description = mdDoc ''
-          Flags for `styli.sh` (list of string).
-          (See [`styli.sh` documentation](https://github.com/thevinter/styli.sh))
-        '';
-        example = literalExpression ''[ "-w 3840" "-h 2160" ]'';
-      };
-
-      refreshInterval = mkOption {
-        default = "0";
-        type = str;
-        description = mdDoc ''
-          If set, `styli.sh` will be executed with this interval to refresh the wallpaper.
-          (Interval in the format of [`systemd.time`](https://www.freedesktop.org/software/systemd/man/systemd.time.html))
-        '';
-        example = literalExpression "30min";
-      };
-
     };
-  };
 
-  ###### implementation
+    ###### implementation
 
-  config = mkIf config.services.stylish.enable {
+    config = mkIf config.services.stylish.enable {
+      assertions = [
+        (hm.assertions.assertPlatform "services.stylish" pkgs platforms.linux)
+      ];
 
-    assertions = [
-      (hm.assertions.assertPlatform "services.stylish" pkgs platforms.linux)
-    ];
+      # include in user env for manual execution
+      # TODO: (for fun) make included optional packages dependent on plugins command line
+      # (or improve plugin option interface of module)
+      # Or: (simpler): add option for extra packages (seems to be also some kind of convention already)
+      # And: add service dependencies (so we can e.g. make sure that hyprpaper service is started before stylish with hyprpaper plugin runs)
+      home.packages = [cfg.package];
 
-    # include in user env for manual execution
-    # TODO: (for fun) make included optional packages dependent on plugins command line
-    # (or improve plugin option interface of module)
-    # Or: (simpler): add option for extra packages (seems to be also some kind of convention already)
-    # And: add service dependencies (so we can e.g. make sure that hyprpaper service is started before stylish with hyprpaper plugin runs)
-    home.packages = [ cfg.package ];
+      systemd.user.services.stylish = {
+        Unit = {
+          Description = "styli.sh set wallpaper service";
+          After = ["graphical.target"];
+        };
 
-    systemd.user.services.stylish = {
-      Unit = {
-        Description = "styli.sh set wallpaper service";
-        After = [ "graphical.target" ];
-      };
-
-      Service = {
-        Type = "oneshot";
-        ExecStart = "${cfg.package}/bin/styli.sh ${
+        Service = {
+          Type = "oneshot";
+          ExecStart = "${cfg.package}/bin/styli.sh ${
             prefixQuotedValOrEmptyNull cfg.search "-s "
           } ${
-            if cfg.flags != null then (concatStringsSep " " cfg.flags) else ""
+            if cfg.flags != null
+            then (concatStringsSep " " cfg.flags)
+            else ""
           }";
+          environment = cfg.env;
+        };
+
+        Install = {WantedBy = ["graphical.target"];};
       };
 
-      Install = { WantedBy = [ "graphical.target" ]; };
+      systemd.user.timers.stylish = {
+        # enable = cfg.refreshInterval != "0";
+        Unit = {Description = "Run styli.sh at refresh interval";};
 
-    };
+        Install = {WantedBy = ["timers.target"];};
 
-    systemd.user.timers.stylish = {
-      # enable = cfg.refreshInterval != "0";
-      Unit = { Description = "Run styli.sh at refresh interval"; };
-
-      Install = { WantedBy = [ "timers.target" ]; };
-
-      Timer = {
-
-        OnUnitInactiveSec = cfg.refreshInterval;
-        Unit = "stylish.service";
+        Timer = {
+          OnUnitInactiveSec = cfg.refreshInterval;
+          Unit = "stylish.service";
+        };
       };
     };
-  };
-}
+  }
